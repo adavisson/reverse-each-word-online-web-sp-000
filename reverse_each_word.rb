7f16eb208037a069262e6f121964c12c96545a07
Andrew Davisson <akdavisson4@gmail.com>
2019-06-10 00:12:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,12 @@
 def reverse_each_word(str)
   arr = str.split(" ")
   rev = ""
-  arr.each do |item|
-    rev << "#{item.reverse} "
+  arr.each_with_index do |item,idx|
+    if idx = arr.length - 1
+      rev << "#{item.reverse}"
+    else
+      rev << "#{item.reverse} "
+    end
   end
   rev
 end
